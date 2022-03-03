255bb9e9e3ddaf5bbc5087a8bf1a898d2d545635
jimenagarciahurtado <j.garciahu.2021@alumnos.urjc.es>
2022-03-03 16:43:10 +0100

    sesion 4

---
+++ session-01/count.py
@@ -0,0 +1,7 @@
+exit = False
+number = 0
+while not exit and number <= 20:
+    print(number)
+    number += 1
+
+
---
+++ session-01/sum20.py
@@ -0,0 +1,7 @@
+sum = 0
+for i in range(1, 21):
+    sum += i
+
+print("Total sum: ", sum)
+
+
