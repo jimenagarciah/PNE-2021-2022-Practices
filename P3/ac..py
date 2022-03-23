0b9ff2c20ad19fd9d8e8234bbe308fb2345987eb
jimenagarciahurtado <j.garciahu.2021@alumnos.urjc.es>
2022-03-23 16:16:35 +0100

    update

--- P3/ac..py
+++ P3/ac..py
@@ -1,8 +1,8 @@
 elif command == "LEN":
-if len(slices) == 1:
-    sequence = Seq()
-else:
-    bases = slices[1]
-    sequence = Seq(bases)
+    if len(slices) == 1:
+        sequence = Seq()
+    else:
+        bases = slices[1]
+        sequence = Seq(bases)
 
-response = f"{sequence.len()}\n"
+    response = f"{sequence.len()}\n"
