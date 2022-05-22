635ef070d420e4e46b9a9790ee57f32618dee635
jimenagarciahurtado <j.garciahu.2021@alumnos.urjc.es>
2022-05-23 00:17:01 +0200

    update

---
+++ session-14/webserver-EX01.py
@@ -0,0 +1,37 @@
+import http.server
+import socketserver
+import termcolor
+
+PORT = 8080
+socketserver.TCPServer.allow_reuse_address = True
+
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        termcolor.cprint(self.requestline, 'green')
+        if self.path == "/":
+            contents = "Welcome to my server"
+            self.send_response(200)
+        else:
+            contents = "Resource not available"
+            self.send_response(404)
+
+        self.send_header('Content-Type', 'text/plain')
+        self.send_header('Content-Length', str(len(contents.encode())))
+        self.end_headers()
+
+        # Send the response message
+        self.wfile.write(contents.encode())
+
+        return
+
+with socketserver.TCPServer(("", PORT), TestHandler) as httpd:
+
+    print("Serving at PORT", PORT)
+
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stoped by the user")
+        httpd.server_close()
